23193513c74aa0a29a5ba2e9c709c9d45aa9d10e
Gergely Karolyi <33122882+gkarolyi@users.noreply.github.com>
2023-12-29 12:14:19 +1100

    filesystem isolation with chroot

--- app/main.rb
+++ app/main.rb
@@ -1,23 +1,33 @@
-require "open3"
+# frozen_string_literal: true
+
+require 'open3'
 require 'tmpdir'
 require 'fileutils'
-require 'pathname'
 
 command = ARGV[2]
 args = ARGV[3..]
+work_dir_name = File.dirname(command)
 
-Dir.mktmpdir(nil, '.') do |dir|
-  work_dir = Pathname.new(File.join(dir, command))
+begin
+  dir = Dir.mktmpdir
+
+  work_dir = File.join(dir, work_dir_name)
+  proc_dir = File.join(dir, 'proc')
+
   FileUtils.mkdir_p(work_dir)
+  FileUtils.mkdir_p(proc_dir)
 
   FileUtils.cp(command, work_dir)
+  system("mount -t proc none #{proc_dir}")
 
+  Dir.chdir(dir)
   Dir.chroot(dir)
 
   stdout, stderr, status = Open3.capture3(command, *args)
 
-  STDOUT.write(stdout)
-  STDERR.write(stderr)
-
+  $stdout.write(stdout)
+  $stderr.write(stderr)
+ensure
+  FileUtils.rm_rf(dir)
   exit(status.exitstatus)
 end

cd61b5865cbcbb22f2444d9880c9981a9568b6a1
Gergely Karolyi <33122882+gkarolyi@users.noreply.github.com>
2024-01-15 15:10:14 +1100

    use fiddle to interface with unshare syscall and create new PID namespace

--- app/main.rb
+++ app/main.rb
@@ -1,12 +1,20 @@
 # frozen_string_literal: true
 
-require 'open3'
 require 'tmpdir'
 require 'fileutils'
+require 'fiddle'
+
+CLONE_NEWPID = 0x20000000
+unshare = Fiddle::Function.new(
+  Fiddle::Handle::DEFAULT['unshare'],
+  [Fiddle::TYPE_INT],
+  Fiddle::TYPE_INT
+)
 
 command = ARGV[2]
 args = ARGV[3..]
 work_dir_name = File.dirname(command)
+
 
 begin
   dir = Dir.mktmpdir
@@ -23,11 +31,19 @@ begin
   Dir.chdir(dir)
   Dir.chroot(dir)
 
-  stdout, stderr, status = Open3.capture3(command, *args)
 
-  $stdout.write(stdout)
-  $stderr.write(stderr)
+  new_pid = unshare.call(CLONE_NEWPID)
+
+  if new_pid != 0
+    puts "Error: Failed to create new PID namespace"
+    exit 1
+  end
+
+  pid = Process.spawn(command, *args)
+  _, status = Process.wait2(pid)
+
 ensure
   FileUtils.rm_rf(dir)
-  exit(status.exitstatus)
 end
+
+exit(status.exitstatus)
